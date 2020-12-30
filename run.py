fdfa67dfb3cac16e81b1006c27bea4ac72a88548
JensdeVlaming <25007405+JensdeVlaming@users.noreply.github.com>
2020-12-30 12:31:30 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,23 @@
+import pysftp
+
+# ADDRESS OF SERVER
+ip = '<HOST>'
+
+# USERNAME OF SERVER
+username = '<USERNAME>'
+
+# PASSWORD OF SERVER
+password = '<PASSWORD>'
+
+with pysftp.Connection(host=ip, username=username, password=password) as sftp:
+    
+    print('Connection succesfully stablished ... ')
+    
+    # File on localhost
+    fileLocal = input('Define the file that you want to upload from your local directorty: ')
+
+    # Path waar het bestand moet komen te staan
+    pathServer = input('Define the remote path where the file will be uploaded: ')
+
+    sftp.put(fileLocal, pathServer)
+    print('Succesfully uploaded file from local directory to remote server ... ')
